8d1afde36def790c23f1b6b95bcf12fd5d4fae1e
Janay Ma <janay.ma7@gmail.com>
2023-06-30 23:19:41 -0700

    move other helper functions to helper module

--- helpers.js
+++ helpers.js
@@ -7,4 +7,40 @@ const getUserByEmail = (email, database) => {
   }
 };
 
-module.exports = { getUserByEmail };
+//takes in user id, returns user associated with id, or undefined if no such user exits
+const getUserById = (id, database) => {
+  return database[id];
+};
+
+//returns a randomly generated 6-character alphanumeric string
+const generateRandomString = () => {
+  // declare all alphanumeric characters
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+  let result = Array(6);
+  //populate array with random characters
+  for (let i = 0; i < 6; i++) {
+    result[i] = characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+  return result.join('');
+};
+
+const urlsForUser = (userID, urlDatabase) => {
+  let userUrls = {};
+  
+  for (const urlID in urlDatabase) {
+    if (urlDatabase[urlID].userID === userID) {
+      userUrls[urlID] = urlDatabase[urlID];
+    }
+  }
+  
+  return userUrls;
+};
+
+module.exports = { 
+  getUserByEmail,
+  getUserById,
+  urlsForUser,
+  generateRandomString
+ };
